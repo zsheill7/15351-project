eaf01b570fd150f90ea68ed19e6755ef7209b721
Zoe Sheill <zsheill7@gmail.com>
2019-05-02 16:03:51 -0400

    updated python pose estimation

--- serial_test.py
+++ serial_test.py
@@ -1,8 +1,12 @@
 import serial
 import time
 
+serial_port = "/dev/cu.usbmodem14101"
 
+sendSerial = serial.Serial(serial_port, 9600)
 
-sendSerial = serial.Serial("COM3", 9600)
 time.sleep(2)
-sendSerial.write(123)
+
+#int headX, int headY, int neckX, int neckY, int rShouX, int rShouY, int rElboX, int rElboY, int rHandX, int rHandY, int pelvX, int pelvY
+
+sendSerial.write(str.encode('15\n5\n15\n9\n12\n12\n8\n12\n5\n14\n20\n30'))
